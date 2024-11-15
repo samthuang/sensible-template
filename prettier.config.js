e5b7f9c6bfb3e8398b25e7135cd31f390042866c
Sam Huang <35948805+samthuang@users.noreply.github.com>
2024-11-15 14:06:48 +0800

    feat: prettier config with tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// Copy and pasted from RedwoodJS prettier default config
+// https://prettier.io/docs/en/options.html
+/** @type {import('prettier').RequiredOptions} */
+module.exports = {
+  trailingComma: "es5",
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
